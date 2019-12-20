3a2fbfdba95110d52f37d5d1611182bbb8a073ce
Nour ElGafy <nour-elgafy@outlook.com>
2019-12-20 17:10:33 +0200

    finished add page

---
+++ MobileApp/MobileApp/Helpers/NullOrEmptyStringChecker.cs
@@ -0,0 +1,19 @@
+﻿namespace MobileApp.Helpers
+{
+    public static class NullOrEmptyStringChecker
+    {
+        public static bool HasNullOrEmptyStrings<T>(T obj)
+        {
+            if (obj == null) return true;
+            foreach (var propertyInfo in typeof(T).GetProperties())
+            {
+                if (!propertyInfo.CanRead) continue;
+                if (propertyInfo.PropertyType != typeof(string)) continue;
+                var val = (string) propertyInfo.GetValue(obj);
+                if (string.IsNullOrWhiteSpace(val)) return true;
+            }
+
+            return false;
+        }
+    }
+}
--- MobileApp/MobileApp/PageModels/Admin/AddEditProductPageModel.cs
+++ MobileApp/MobileApp/PageModels/Admin/AddEditProductPageModel.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.IO;
 using Acr.UserDialogs;
+using MobileApp.Helpers;
 using MobileApp.Models.DataModels;
 using Plugin.ImageEdit;
 using Plugin.Media;
@@ -13,16 +14,6 @@ namespace MobileApp.PageModels.Admin
 {
     public class AddEditProductPageModel : BasePageModel
     {
-        public ShoppingItem NewItem { get; set; }
-        public Command PickPhotoCommand { get; set; }
-        public Command InsertNewItemCommand { get; set; }
-        public ImageSource SelectedImage { get; set; }
-        public string ImageName { get; set; }
-        public string ButtonText { get; set; }
-        public bool Visibility { get; set; }
-        ObservableCollection<Category> Categories { get; set; }
-        Category SelectedCategory { get; set; }
-
         public AddEditProductPageModel()
         {
             Title = "Manage Your Product";
@@ -31,22 +22,55 @@ namespace MobileApp.PageModels.Admin
             PickPhotoCommand = new Command(PickPhotoExcute);
             InsertNewItemCommand = new Command(InsertNewItemExecute);
             NewItem = new ShoppingItem();
-            Categories = new ObservableCollection<Category>();
             SelectedCategory = new Category();
             Visibility = false;
+            Categories = new List<Category>
+            {
+                new Category {Id = 1, Name = "Food"},
+                new Category {Id = 2, Name = "Water"},
+                new Category {Id = 3, Name = "Air"}
+            };
         }
 
-        protected override void ViewIsAppearing(object sender, EventArgs e)
-        {
-            base.ViewIsAppearing(sender, e);
-            Categories.Add(new Category(){Id = 1,Name = "Food"});
-            Categories.Add(new Category(){Id = 2,Name = "Water"});
-            Categories.Add(new Category(){Id = 3,Name = "Air"});
-        }
+        public ShoppingItem NewItem { get; set; }
+        public Command PickPhotoCommand { get; set; }
+        public Command InsertNewItemCommand { get; set; }
+        public ImageSource SelectedImage { get; set; }
+        public string ImageName { get; set; }
+        public string ButtonText { get; set; }
+        public bool Visibility { get; set; }
+        public List<Category> Categories { get; set; }
+        public Category SelectedCategory { get; set; }
 
         private void InsertNewItemExecute()
         {
+            if (!IsItemValid())
+                return;
+            if (InsertNewItem())
+            {
+
+            }
+            //
+        }
+
+        private bool InsertNewItem()
+        {
             throw new NotImplementedException();
+        }
+
+        private bool IsItemValid()
+        {
+            if (NullOrEmptyStringChecker.HasNullOrEmptyStrings(NewItem))
+            {
+                UserDialogs.Instance.Alert("Please check your input", "Invalid Data");
+                return false;
+            }
+            else if (NewItem.Price <= 0)
+            {
+                UserDialogs.Instance.Alert("Please check your price", "Invalid Data");
+                return false;
+            }
+            return true;
         }
 
         private void PickPhotoExcute()
@@ -73,15 +97,15 @@ namespace MobileApp.PageModels.Admin
                     selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOption);
                     if (selectedImageFile != null)
                     {
-                        ImageName = selectedImageFile.Path;
                         var image = await CrossImageEdit.Current.CreateImageAsync(selectedImageFile.GetStream());
                         var width = 700;
                         var height = 700;
                         var data = image.Resize(width, height).ToPng();
                         SelectedImage = ImageSource.FromStream(() => new MemoryStream(data));
+                        ImageName = selectedImageFile.Path;
                         NewItem.Logo = Convert.ToBase64String(data);
-                        var st = ImageName.Split('/');
-                        ImageName = st[st.Length - 1];
+                        //var st = ImageName.Split('/');
+                        //ImageName = st[st.Length - 1];
                         Visibility = true;
                     }
                 }
