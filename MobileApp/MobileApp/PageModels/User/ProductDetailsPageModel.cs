420365730a0efd6f418331826a1b5a07f5c0329e
Nour ElGafy <nour-elgafy@outlook.com>
2019-12-22 03:38:13 +0200

    product Details page added

---
+++ MobileApp/MobileApp/PageModels/User/ProductDetailsPageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MobileApp.Models.DataModels;
+
+namespace MobileApp.PageModels.User
+{
+    public class ProductDetailsPageModel : BasePageModel
+    {
+        public ShoppingItemModel Item { get; set; }
+        public ProductDetailsPageModel()
+        {
+            Title = Item.Name;
+        }
+
+        public override Task Init(object initData)
+        {
+            ShoppingItemModel shoppingItem = (ShoppingItemModel)initData;
+            Item = shoppingItem;
+            return base.Init(initData);
+        }
+    }
+}
---
+++ MobileApp/MobileApp/Pages/User/ProductDetailsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Pages.User
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductDetailsPage
+    {
+        public ProductDetailsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
